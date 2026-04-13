ff6e74e7d64d6d73c57e2c74b224fad7f721e71c
BogdanATA <batameev@gmail.com>
2026-04-13 13:20:11 -0400

    Created the gather user info section

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //gather user info
+        System.out.println("Enter your name: ");
+        String firstName = scanner.nextLine().trim();
+
+        System.out.println("Enter you Middle name: ");
+        String middleName = scanner.nextLine().trim();
+
+        System.out.println("Enter your Last name: ");
+        String lastName = scanner.nextLine().trim();
+
+        System.out.println("Enter your suffix: ");
+        String suffix = scanner.nextLine().trim();
+
+
+    }
 }
